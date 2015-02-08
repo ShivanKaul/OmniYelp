f291fbf8089f84f8c060de0b5c6b07328eb07920
ShivanKaul <shivankaul.1993@gmail.com>
2015-02-08 13:46:13 -0500

    Made the thing, basically

---
+++ extension/src/background.js
@@ -0,0 +1,22 @@
+/* 
+	function navigate
+	- navigates current tab to Yelp with search terms
+	- used by omnibox
+ */
+
+function navigate(inputString) {
+	if(inputString=="") {
+		// If empty, then go to Yelp
+		chrome.tabs.create({"url" : "https://www.yelp.com", "active" : true});
+	} else {
+		var inputURI = encodeURIComponent(inputString);	
+		chrome.tabs.getSelected( undefined, function(tab) {
+			chrome.tabs.update(tab.id, {url: "https://http://www.yelp.com/search?find_desc="+inputURI+"&src=opensearch"}, undefined);
+		}); 
+	}
+}
+
+
+// Reacting to users entering "yp" in the omnibox
+chrome.omnibox.onInputEntered.addListener(navigate);
+chrome.omnibox.setDefaultSuggestion({"description" : "Search Yelp for '%s'"});
